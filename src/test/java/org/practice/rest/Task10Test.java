98d3d96680057231817f5b4b29e73e7e07665d07
abondarev-sel <abondarev@determine.com>
2017-09-27 19:49:03 +0300

    add checkNewServer and wait5Minutes + stubs for verifyApplication and cleanUpNewServer

--- src/test/java/org/practice/rest/Task10Test.java
+++ src/test/java/org/practice/rest/Task10Test.java
@@ -3,14 +3,19 @@ package org.practice.rest;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 
 public class Task10Test extends BaseTest {
+
+    private String newServerIP = "1.2.3.4";
+
 
     protected void setUpEndPoint() {
         endpoint = ROOT_URL + PORT + CUSTOMERS_RES;
@@ -55,7 +60,6 @@ public class Task10Test extends BaseTest {
 
     @Test(description = "Register new server for existing customer service instance")
     public void registerNewServer(){
-        String newServerIP = "1.2.3.4";
         String call = ROOT_URL + PORT + CUSTOMERS_RES + CUSTOMER
                 + SERVICE_RES + SERVICE + "deployments/" + newServerIP;
         String body = "instanceId=apiTest";
@@ -66,5 +70,44 @@ public class Task10Test extends BaseTest {
                 .filters(new ResponseLoggingFilter(), new RequestLoggingFilter())
             .when().put(call)
             .then().statusCode(expectedStatus);
+
+    }
+
+    @Test(description = "Verify that the server is registered in the controller for the specific customer and\n" +
+            "service instance", dependsOnMethods = "registerNewServer")
+    public void checkNewServer(){
+        int expectedStatus = 200;
+        String call = ROOT_URL + PORT + CUSTOMERS_RES + CUSTOMER
+                + SERVICE_RES + SERVICE + "deployments/";
+
+        String allServers = getBodyAsString(
+                given().filters(new ResponseLoggingFilter(), new RequestLoggingFilter())
+                        .when().get(call)
+                        .then().statusCode(expectedStatus));
+
+        Assert.assertTrue(allServers.contains(newServerIP),
+                newServerIP + " should be shown in " + allServers);
+    }
+
+    @Test(description = "Wait for 5 minutes", dependsOnMethods = "checkNewServer", enabled = false)
+    public void wait5Minutes(){
+        try {
+            TimeUnit.MINUTES.sleep(5);
+        } catch (InterruptedException ex) {
+            System.out.println("Failed to wait for 5 minutes");
+            ex.printStackTrace();
+        }
+        System.out.println("Waited for 5 minutes");
+    }
+
+    @Test(description = "Verify that application on the newly registered server is accessible"
+    , dependsOnMethods = "checkNewServer", alwaysRun = true)
+    public void verifyApplication(){
+        Assert.assertTrue(true, "Need to clarify");
+    }
+
+    @Test(description = "Clean up (remove the server from the Controller)", dependsOnMethods = "verifyApplication")
+    public void cleanUpNewServer(){
+        Assert.assertTrue(true, "Need to clarify");
     }
 }
